ac41510625b863636a9a1fc76d90e1ec5073c915
kyle roux <kyle@level2designs.com>
2015-06-21 16:55:08 -0700

    fixs

--- phlaskr/mk_local_settings.sh
+++ phlaskr/mk_local_settings.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo -n 'class LocalConfig(object):\n\tpass\n' > local_settings.py
+#echo -n 'class LocalConfig(object):\n\tpass\n' > local_settings.py
+rm local_settings.py
+vi local_settings.py <<x23LimitStringx23
+i
+class LocalConfig(object):
+    pass
+
+wq!
+x23LimitStringx23
+exit
 
