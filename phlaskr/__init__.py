afbf83aaf0ef6f2cba9d4694effdd4f2b34ed678
kyle roux <kyle@level2designs.com>
2015-06-22 01:25:44 -0700

    fixing tests

--- phlaskr/__init__.py
+++ phlaskr/__init__.py
@@ -2,7 +2,14 @@ from api import api
 from front import front
 from werkzeug.wsgi import DispatcherMiddleware
 from werkzeug import run_simple
-from gevent.pywsgi import WSGIServer
+import os
+
+
+runserver = lambda app,host,port: run_simple(host,port,app,use_debugger=True,use_reloader=True)
+if not os.environ.get('TESTING'):
+    from gevent.pywsgi import WSGIServer
+    run_server = lambda app,host,port: WSGIServer((host,port),app).serve_forever()
+
 
 #front.config['DATABASE_URI'] = 'sqlite:///test3.db'
 #api.config['DATABASE_URI'] = 'sqlite:///test3.db'
@@ -11,9 +18,6 @@ application = DispatcherMiddleware(front,{
     '/api/v1':api
 })
 
-print api.config.get('DATABASE_URI')
-print front.config.get('DATABASE_URI')
+
 if __name__ == "__main__":
-    server = WSGIServer(('0.0.0.0',8000),application)
-    server.serve_forever()
-    #run_simple('0.0.0.0',8000,application,use_debugger=True,use_reloader=True)
+    run_server('0.0.0.0',8000,application)
