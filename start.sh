7a3bb742550f350f584fa6b2d0e4dcca8f954e0a
kyle roux <kyle@level2designs.com>
2015-06-25 18:06:05 -0700

    fix

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ gunicorn -k gevent\
          -t $TIMEOUT\
          --keep-alive $KEEPALIVE\
          --workers $WORKERS\
-         $PID_ARG "$APP_PID_FILE"\
-         $LOG_ARG "$APP_LOG_FILE"\
+         $PID_ARG $APP_PID_FILE\
+         $LOG_ARG $APP_LOG_FILE\
          $PRELOAD\
-         --bind 127.0.0.1:$PORT"\
+         --bind 127.0.0.1:$PORT\
          phlaskr:application
