62c0dad1e64a8f51d35faa7f355c8e41fc01e109
kyle roux <kyle@level2designs.com>
2015-06-25 17:38:30 -0700

    trying to chnage webapp to gunicorn/nginx

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-python phlaskr/__init__.py
+gunicorn -k gevent\
+         -t $TIMEOUT\
+         --keep-alive $KEEPALIVE\
+         --workers $WORKERS\
+         $PID_ARG "$APP_PID_FILE"\
+         $LOG_ARG "$APP_LOG_FILE"\
+         $PRELOAD\
+         --bind 127.0.0.1:$PORT"\
+         phlaskr:application
